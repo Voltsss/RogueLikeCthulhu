a00b62dbd1c0cdff4edca10c40a5c70332132fb6
Aoino <aoiro.aoino@gmail.com>
2014-07-26 20:43:44 +0900

    add CharacterParamter methods in Character.scala

--- src/main/scala/model/Character.scala
+++ src/main/scala/model/Character.scala
@@ -1,6 +1,6 @@
 package model
 
-trait Charactor {
+trait Character {
 
   final case class Position(var x: Int, var y: Int)
   val position: Position
@@ -20,6 +20,34 @@ trait Charactor {
   def moveUp    = setY(position.y - 1)
   def moveDown  = setY(position.y + 1)
 
-}
 
+  final case class CharacterParameter(
+    var experience: Int = 0,
+    var hitpoint:   Int = 0,
+    var attack:     Int = 0,
+    var defence:    Int = 0,
+    var agility:    Int = 0,
+    var dexterity:  Int = 0,
+    var sanity:     Int = 0
+  )
+  val cp: CharacterParameter
+
+  def setExperience(v: Int) = cp.experience = v
+  def setHitpoint(v: Int)   = cp.hitpoint = v
+  def setAttack(v: Int)     = cp.attack = v
+  def setDefence(v: Int)    = cp.defence = v
+  def setAgility(v: Int)    = cp.agility = v
+  def setDexterity(v: Int)  = cp.dexterity = v
+  def setSanity(v: Int)     = cp.sanity = v
+
+  def getExperience = cp.experience
+  def getHitpoint   = cp.hitpoint
+  def getAttack     = cp.attack
+  def getDefence    = cp.defence
+  def getAgility    = cp.agility
+  def getDexterity  = cp.dexterity
+  def getSanity     = cp.sanity
+
+  def getLevel = cp.experience / 10
+}
 
