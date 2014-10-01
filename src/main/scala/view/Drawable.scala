500b7f5103e6274bc9f51e96b0e14ff2bfddfd82
Volts <svoltsss@gmail.com>
2014-10-02 01:46:13 +0900

    DrawableTrait and Some Method implements

---
+++ src/main/scala/view/Drawable.scala
@@ -0,0 +1,33 @@
+package view
+
+
+/**
+ * Created by volts on 14/09/28.
+ */
+trait Drawable {
+  type Screen = Array[Array[Option[String]]]
+  case class DummyPosition(y:Int, x:Int)
+  def draw( exScreen: Screen ) : Screen
+
+  def overwritePositions ( pList : Array[DummyPosition], exScreen : Screen , char : Char) : Screen = {
+    if ( pList.isEmpty ){
+      exScreen
+    }else{
+      overwritePositions(pList.tail,exScreen,char).updated(pList.head.x,exScreen(pList.head.x).updated(pList.head.y,Some(char.toString)))
+    }
+  }
+
+  def screenEmpty ( width : Int , height: Int ) : Screen = {
+    Array.fill(width)(Array.fill(height)(Option.empty[String]))
+  }
+
+  def screenFill ( width : Int , height: Int , str : String ) : Screen = {
+    Array.fill(width)(Array.fill(height)( Some( str )))
+  }
+
+  def appearance(sc : Screen): Unit ={
+
+  }
+  
+
+}
