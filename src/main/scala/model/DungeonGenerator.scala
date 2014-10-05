119be5e07665e7c76d24ec40752708b6ff986a85
Aoino <aoiro.aoino@gmail.com>
2014-10-05 19:26:34 +0900

    Refactoring DungeonGenerator

--- src/main/scala/model/DungeonGenerator.scala
+++ src/main/scala/model/DungeonGenerator.scala
@@ -3,32 +3,56 @@ package model
 import scala.io.Source
 import model.param.PanelParam._
 
+trait Generatable {
+  type Field
+
+  def make: Field
+}
+
+trait GeneratorFromFile extends Generatable {
+  type Field = Array[Array[Panel]]
+
+  val fileName: String
+
+  private val replaceTable: Map[String, Panel] = Map(
+    "1" -> Floor,
+    "2" -> Wall,
+    "3" -> Door,
+    "4" -> Way
+  )
+
+  private def replaceChar2Panel(str: String): Array[Panel] =
+    str.map{(c: Char) => replaceTable.getOrElse(c.toString, NothingPanel)}.toArray
+
+  def make: Field = {
+    val file = getClass.getResourceAsStream(fileName)
+    Source.fromInputStream(file).getLines.map(replaceChar2Panel).toArray
+  }
+}
+
+trait GeneratorRandom extends Generatable {
+  type Field = Array[Array[Panel]]
+
+  def make: Field = Array.ofDim[Panel](1, 0)
+}
+
+class TestDungeon extends GeneratorFromFile {
+  val fileName = "/test_dungeon.dun"
+}
+
+class RandomDungeon extends GeneratorRandom {
+}
+
+
+// If you create dungeon, use this object.
 object DungeonGenerator {
 
-  val replaceRoguePanel: String => Array[Panel] = (str: String) => {
-    str map {
-      (_: Char) match {
-        case '1' => Floor
-        case '2' => Wall
-        case '3' => Door
-        case '4' => Way
-        case _   => NothingPanel
-      }
-    }
-  }.toArray
+  def makeTestDungeon = new TestDungeon make
 
-  private def makeDungeon(fileName: String): Array[Array[Panel]] = {
-    val file = getClass.getResourceAsStream(fileName)
-    Source.fromInputStream(file).getLines.map(replaceRoguePanel).toArray
-  }
-
-  // Test Dungeon
-  def makeTestDungeon = makeDungeon("/test_dungeon.dun")
-
-  // TODO: Randomize Dungeon
-  def makeRandomDungeon = Array.ofDim[Panel](1,0)
+  def makeRandomDungeon = new RandomDungeon make
 
   implicit class dungeon2String(dungeon: Array[Array[Panel]]) {
     def toAppearance: String = dungeon.map {_.map(_.appearance).mkString}.mkString("\n")
   }
 }
+
