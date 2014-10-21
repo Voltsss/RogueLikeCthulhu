267d22d51b171870cf7bfd198788c5525999d57f
Aoino <aoiro.aoino@gmail.com>
2014-10-21 22:43:06 +0900

    improve package

--- src/main/scala/model/param/PanelParam.scala
+++ src/main/scala/model/param/PanelParam.scala
@@ -2,10 +2,10 @@ package model.param
 
 trait PanelParam {
   abstract sealed class Panel(s: String)
-  case object Wall  extends Panel("Wall")
-  case object Door  extends Panel("Door")
-  case object Way   extends Panel("Way")
-  case object Floor extends Panel("Floor")
+  case object Wall         extends Panel("Wall")
+  case object Door         extends Panel("Door")
+  case object Way          extends Panel("Way")
+  case object Floor        extends Panel("Floor")
   case object NothingPanel extends Panel("NothingPanel")
 
   implicit class PanelParam2String(p: Panel) {
@@ -19,4 +19,5 @@ trait PanelParam {
   }
 }
 
-object PanelParam extends PanelParam
+object panel extends PanelParam
+
